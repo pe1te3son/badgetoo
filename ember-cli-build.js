c2663efcb50bc8e5ae2e3a50376dd91e0606f953
Peter Janak <2010janak2010@gmail.com>
2016-11-16 04:15:23 +0000

    feat: Install pickadate - materialize

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -27,6 +27,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/picker.js');
+  app.import('vendor/picker.date.js');
+  app.import("vendor/picker.time.js");
   app.import("bower_components/material-design-lite/material.js");
   return app.toTree();
 };
