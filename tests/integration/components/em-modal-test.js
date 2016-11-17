8f0c3945b13bb70fc55c22658c8043ef16b4d645
Peter Janak <2010janak2010@gmail.com>
2016-11-17 04:13:03 +0000

    feat: Add modal component

---
+++ tests/integration/components/em-modal-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('em-modal', 'Integration | Component | em modal', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{em-modal}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#em-modal}}
+      template block text
+    {{/em-modal}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
