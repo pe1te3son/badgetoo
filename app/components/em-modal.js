19422fea2ffc9461ee636339ac3ebbfb9da185d2
Peter Janak <2010janak2010@gmail.com>
2016-11-18 01:25:30 +0000

    feat: Add modal animation

--- app/components/em-modal.js
+++ app/components/em-modal.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+  didInsertElement () {
+    const $emEl = this.$();
+    const $modalBtn = $('[em-modal-open]');
+
+    $modalBtn.click(() => {
+      $emEl.fadeIn(100);
+    });
+
+    $emEl.click(event => {
+      if (!$(event.target).parents(`#${this.$().attr('id')}`).length) {
+        $emEl.fadeOut(100);
+      }
+    });
+  }
 });
