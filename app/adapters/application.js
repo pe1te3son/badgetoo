c485cbc06a6291a7c08598198eb8437b9868d6aa
Peter Janak <2010janak2010@gmail.com>
2016-11-19 02:11:15 +0000

    refactor: Cache model to limit number of requests to database

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import LFAdapter from 'ember-localforage-adapter/adapters/localforage';
 
 export default LFAdapter.extend({
-  namespace: 'expenses'
+  namespace: 'expenses',
+  caching: 'model'
 });
