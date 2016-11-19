4afe9bf31317d76f9de7b7cc76812ca0d19cd183
Peter Janak <2010janak2010@gmail.com>
2016-11-19 02:15:58 +0000

    feat: On adding spending to db, update view

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,9 +2,26 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Controller.extend({
+  spendingsMeter: 0,
   init () {
     Ember.run.schedule('afterRender', () => {
+
     });
+  },
+
+  watchAddSpending: function () {
+    this.updateSpendingsMeter();
+  }.observes('model.@each'),
+
+  updateSpendingsMeter () {
+    let sumCounted = 0;
+    this.get('model').forEach(item => {
+      sumCounted += parseInt(item.get('sum'));
+    });
+    this.set('spendingsMeter', sumCounted);
+    return;
+  },
+
     saveRecord (record) {
       let newExpense = this.store.createRecord('expense', record);
       newExpense.save();
