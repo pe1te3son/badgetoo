9d45e5fd70490db8934904c864412ada20a69bb6
Peter Janak <2010janak2010@gmail.com>
2016-11-23 03:10:10 +0000

    feat: Set validation function for min, max, pattern and required

--- app/components/validate-input.js
+++ app/components/validate-input.js
@@ -3,4 +3,26 @@ import $ from 'jquery';
 
 export default Ember.Component.extend({
 
+  emMin (value) {
+    if (this.get('validate')) {
+      return this.get('validate').length < parseInt(value);
+    }
+  },
+
+  emMax (value) {
+    return this.get('validate').length > parseInt(value);
+  },
+
+  emPattern (value) {
+    const reg = new RegExp(`${value}`);
+    if (this.get('validate').length) {
+      return !reg.test(this.get('validate'));
+    }
+  },
+
+  emRequired () {
+    if (this.$().hasClass('is-touched')) {
+      return this.get('validate').length < 1;
+    }
+  }
 });
