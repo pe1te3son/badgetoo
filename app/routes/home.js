00f1d1533dfbcbb34531840aa06a8f170e601966
Peter Janak <2010janak2010@gmail.com>
2016-11-24 02:17:34 +0000

    feat: Set up home controller in route with user  settings

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  userSettings: Ember.inject.service('user-settings'),
+
   model () {
     return this.get('store').findAll('expense');
+  },
+
+  setupController: function (controller, model) {
+   // Call _super for default behavior
+    this._super(controller, model);
+   // Implement your custom setup after
+    this.controllerFor('home').set('currency', this.get('userSettings').currency());
   }
 });
