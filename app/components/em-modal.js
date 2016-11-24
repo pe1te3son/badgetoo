2d1c5f5e42690117bc49ba2efa2d00ce55eda89a
Peter Janak <2010janak2010@gmail.com>
2016-11-24 04:14:22 +0000

    feat: Add trap tab key functionality to modal

--- app/components/em-modal.js
+++ app/components/em-modal.js
@@ -4,18 +4,58 @@ import $ from 'jquery';
 export default Ember.Component.extend({
   attributeBindings: ['role'],
   didInsertElement () {
-    const $emEl = this.$();
+    const $Element = this.$();
     const $modalBtn = $('[em-modal-open]');
 
-    $emEl.attr('role', 'dialog');
+    $Element.attr('role', 'dialog');
     $modalBtn.click(() => {
-      $emEl.fadeIn(100);
+      $Element.fadeIn(100);
+      this.lockBackground($Element);
     });
 
-    $emEl.click(event => {
+    $Element.click(event => {
       if (!$(event.target).parents(`#${this.$().attr('id')}`).length) {
-        $emEl.fadeOut(100);
+        $Element.fadeOut(100);
       }
     });
+  },
+
+  lockBackground (el) {
+    const focusableElementQuery = 'select:not([disabled]), button:not([disabled]), [tabindex="0"], input:not([disabled]), a[href]';
+    const backgroundActiveEl = document.activeElement;
+    const focusableElements = el.find(focusableElementQuery);
+    const firstEl = focusableElements[0];
+    const lastEl = focusableElements[focusableElements.length - 1];
+
+    // Focus first element in modal
+    firstEl.focus();
+    el.keydown(event => {
+      // If Esc pressed
+      if (event.keyCode === 27) {
+        el.fadeOut(100);
+        return backgroundActiveEl.focus();
+      }
+
+      // Trap Tab key while modal open
+      this.trapTabKey(event, firstEl, lastEl);
+    });
+  },
+
+  trapTabKey (event, ...params) {
+    const [ firstEl, lastEl ] = params;
+
+    if (event.keyCode === 9) {
+      if (event.shiftKey) {
+        if (document.activeElement === firstEl) {
+          event.preventDefault();
+          return lastEl.focus();
+        }
+      } else {
+        if (document.activeElement === lastEl) {
+          event.preventDefault();
+          return firstEl.focus();
+        }
+      }
+    }
   }
 });
