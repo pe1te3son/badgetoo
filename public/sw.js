0c74eca8132f6ec295075d262cf327dd0e4c4c23
Peter Janak <2010janak2010@gmail.com>
2016-11-26 03:14:02 +0000

    feat: Add service worker

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+/* eslint-env serviceworker */
+(function () {
+  'use strict';
+  var CACHE_VERSION = 'v3';
+
+  self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open(CACHE_VERSION)
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function (resp) {
+        return resp || fetch(event.request).then(function (response) {
+          caches.open(CACHE_VERSION).then(function (cache) {
+            cache.put(event.request, response.clone());
+          });
+          return response;
+        });
+      }).catch(function () {
+        console.log('not in cache');
+      })
+    );
+  });
+
+  this.addEventListener('activate', function (event) {
+    var cacheWhitelist = [CACHE_VERSION];
+
+    event.waitUntil(
+      caches.keys().then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
+})();
