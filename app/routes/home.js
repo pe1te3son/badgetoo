b38994743077059dc8f04891bc53e13b189deb17
Peter Janak <2010janak2010@gmail.com>
2016-11-30 01:54:56 +0000

    refactor: Setting curency in home controller no longer required

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,16 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  userSettings: Ember.inject.service('user-settings'),
 
   model () {
     return this.get('store').findAll('expense');
-  },
-
-  setupController: function (controller, model) {
-   // Call _super for default behavior
-    this._super(controller, model);
-   // Implement your custom setup after
-    this.controllerFor('home').set('currency', this.get('userSettings').currency());
+    // return this.get('store').findAll('expense')
+    //   .then(response => response.filter(expense => {
+    //     console.log('dfa');
+    //     return moment(expense.get('timestamp')).isBetween('2016-11-05', '2016-11-29');
+    //   }));
   }
 });
