c7fb4a93ab46132535d7162c1811123f002e905e
Peter Janak <2010janak2010@gmail.com>
2016-11-30 04:21:20 +0000

    feat: Send action with id to parent when table cell clicked

--- app/components/expense-table-cell.js
+++ app/components/expense-table-cell.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'tr'
+  tagName: 'tr',
+  attributeBindings: ['tabindex'],
+
+  click () {
+    this.sendAction('action', this.get('expenseId'));
+  },
+
+  keyDown (event) {
+    if (event.which === 13) {
+      this.sendAction('action', this.get('expenseId'));
+    }
+  }
 });
