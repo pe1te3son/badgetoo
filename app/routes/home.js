c6fd27aada1305fbf54e46033fdecc70c2ee86bc
Peter Janak <2010janak2010@gmail.com>
2016-12-01 01:13:40 +0000

    feat: Create store for current month if doesnt exists

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
+import moment from 'npm:moment';
 
 export default Ember.Route.extend({
+
+  beforeModel () {
+    const currentDateId = moment().format('YYYY-MM');
+
+    if (this.store.peekRecord('expenses', currentDateId) === null) {
+      let expensesStore = this.store.createRecord('expenses', {
+        id: currentDateId
+      });
+      expensesStore.save();
+    }
+  },
 
   model () {
     return this.get('store').findAll('expense');
