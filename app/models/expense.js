68141aa18b1be2b2d1543517bc450827e7259445
Peter Janak <2010janak2010@gmail.com>
2016-12-01 00:53:46 +0000

    feat: Set DS belongsTo for expense to contect it with expenses by date

--- app/models/expense.js
+++ app/models/expense.js
@@ -1,8 +1,16 @@
 import DS from 'ember-data';
+import moment from 'npm:moment';
 
 export default DS.Model.extend({
+  expenses: DS.belongsTo('expenses'),
   timestamp: DS.attr('number', {
-    defaultValue () { return Date.now(); }
+    defaultValue () { return moment(/*'2016-11-24', 'YYYY-MM-DD'*/).toDate().getTime(); }
+  }),
+  month: DS.attr('number', {
+    defaultValue () { return parseInt(moment().format('MM')); }
+  }),
+  year: DS.attr('number', {
+    defaultValue () { return parseInt(moment().format('YYYY')); }
   }),
   name: DS.attr('string'),
   category: DS.attr('string'),
