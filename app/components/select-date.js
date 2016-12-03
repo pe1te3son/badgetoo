b20dfdc1ee90da492ca4bd5db5a7c18429749925
Peter Janak <2010janak2010@gmail.com>
2016-12-03 03:07:56 +0000

    feat: Add select element that displays possible time periods

--- app/components/select-date.js
+++ app/components/select-date.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  userSettings: Ember.inject.service(),
+
+  init () {
+    this._super();
+    let periods = this.get('userSettings').timePeriods();
+
+    // Sort by latest on top
+    this.set('timePeriods', periods.sortBy('begins').reverse());
+
+    // Create array of years
+    this.set('years', periods.uniqBy('year'));
+  }
 });
