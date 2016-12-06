c9ed92d0ace4e73567e93e94942ab125b033d035
Peter Janak <2010janak2010@gmail.com>
2016-12-06 23:06:48 +0000

    refactor: Fetch all expenses in route model instead of filtered ones

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
-import moment from 'npm:moment';
 
 export default Ember.Route.extend({
-  userSettings: Ember.inject.service(),
-
   model () {
-    return this.get('store').findAll('expenses')
-      .then(data => {
-        return data.store.findAll('expense');
-      });
+    return this.store.findAll('expense');
   }
 });
