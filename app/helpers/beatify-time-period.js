ecaecf241adff124f517163c6db00cb5030a99ba
Peter Janak <2010janak2010@gmail.com>
2016-12-07 03:03:43 +0000

    refactor: Beatify-time-period helper returns month fully spelled

--- app/helpers/beatify-time-period.js
+++ app/helpers/beatify-time-period.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import moment from 'npm:moment';
 
 export function beatifyTimePeriod (params) {
-  return moment(params[0].begins).format('DD MMM') + ' - ' + moment(params[0].ends).format('DD MMM');
+  return moment(params[0], 'MM').format('MMMM');
 }
 
 export default Ember.Helper.helper(beatifyTimePeriod);
