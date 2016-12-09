62d4fd3cbb3337c11604790a58083efd817964b9
Peter Janak <2010janak2010@gmail.com>
2016-12-09 02:16:14 +0000

    feat: Add mdl-lite snackbar to notify user of required reload.
    Reload required when user changes a settings

--- app/controllers/home/settings.js
+++ app/controllers/home/settings.js
@@ -7,6 +7,25 @@ export default Ember.Controller.extend({
     });
   },
 
+  settingsSnackbar (message) {
+    const _this = this;
+    const snackbarContainer = document.querySelector('#snackbar-container');
+
+    const handler = function () {
+      _this.transitionToRoute('home');
+      Ember.run.later(() => {
+        window.location.reload(true);
+      }, 500);
+    };
+
+    const data = {
+      message,
+      timeout: 4000,
+      actionHandler: handler,
+      actionText: 'Reload'
+    };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+  },
   currency: function () {
     let currency = this.get('model').findBy('id', 'currency');
     if (!currency) {
