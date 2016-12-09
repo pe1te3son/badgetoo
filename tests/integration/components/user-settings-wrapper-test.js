06849c445571ee62e8fb9803e909807fdf2f6fcc
Peter Janak <2010janak2010@gmail.com>
2016-12-09 02:27:05 +0000

    feat: Add component for all user settings

---
+++ tests/integration/components/user-settings-wrapper-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('user-settings-wrapper', 'Integration | Component | user settings wrapper', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{user-settings-wrapper}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#user-settings-wrapper}}
+      template block text
+    {{/user-settings-wrapper}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
