2dd4242b69e02766d4ae7158607592bbe53f59b5
Peter Janak <2010janak2010@gmail.com>
2016-12-10 01:48:24 +0000

    feat: Add currency exchange path to router

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function () {
   this.route('home', {path: '/'}, function () {
     this.route('settings');
+    this.route('currency-exchange');
   });
 });
 
