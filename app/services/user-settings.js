060a2538f1dbc40c36094276f77eeec8d79c0dc4
Peter Janak <2010janak2010@gmail.com>
2016-12-12 00:15:02 +0000

    refactor: Return symbol when currency method called from service

--- app/services/user-settings.js
+++ app/services/user-settings.js
@@ -4,13 +4,14 @@ export default Ember.Service.extend({
   store: Ember.inject.service(),
 
   currency () {
-    return this.get('store').findRecord('setting', 'currency')
+    return this.get('store').findRecord('setting', 'currency-symbol')
       .then(response => response.get('value'))
       .catch(err => {
         if (typeof err === 'undefined') {
           return '$';
         }
       });
+  },
 
   currencyNames () {
     return ['AUD', 'CAD', 'CHF', 'EUR', 'GBP', 'JPY', 'NZD', 'RUB', 'USD'];
