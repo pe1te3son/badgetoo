447b60a5c3e14e78db1d2ffb76800c19a9f4810b
Peter Janak <2010janak2010@gmail.com>
2016-12-12 22:37:29 +0000

    feat: Dsplay currency rates

--- app/components/currency-rates.js
+++ app/components/currency-rates.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  userSettings: Ember.inject.service(),
+  currenciesFiltered: null,
+
+  didReceiveAttrs () {
+    let data = this.get('data.rates');
+    // Get all currency names and set handlebars usable object
+    this.get('userSettings').currencyNames()
+      .then(response => {
+        let values = [];
+        response.forEach(cur => {
+          values.pushObject({
+            cur,
+            value: data[cur]
+          });
+        });
+        return this.set('currenciesFiltered', values);
+      });
+  }
 });
