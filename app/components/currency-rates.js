8cd1faeed5f802a294a930e03add05a8b37fc490
Peter Janak <2010janak2010@gmail.com>
2016-12-13 02:12:39 +0000

    feat: Calculate and display currency inflation in currency-rates component

--- app/components/currency-rates.js
+++ app/components/currency-rates.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import moment from 'npm:moment';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
   userSettings: Ember.inject.service(),
@@ -10,13 +12,42 @@ export default Ember.Component.extend({
     this.get('userSettings').currencyNames()
       .then(response => {
         let values = [];
-        response.forEach(cur => {
+        response.forEach(currency => {
           values.pushObject({
-            cur,
-            value: data[cur]
+            currency,
+            value: data[currency]
           });
         });
-        return this.set('currenciesFiltered', values);
+        return this.set('latestCurrencyRates', values);
+      })
+      .then(() => {
+        return this.get('userSettings').currentCurrencyName()
+          .then(userCurrencyName => this.fetchPreviousDayCurrencyRates(userCurrencyName));
       });
+  },
+
+  fetchPreviousDayCurrencyRates (userCurrencyName) {
+    return $.ajax(`https://api.fixer.io/${moment().subtract(2, 'day').format('YYYY-MM-DD')}?base=${userCurrencyName}`, {
+      dataType: 'jsonp'
+    })
+      .done(response => {
+        this.calculateInflationRates(response.rates);
+      })
+      .fail(() => {
+        console.log('Failed to fetch previous day rates');
+      });
+  },
+  calculateInflationRates (latesRates) {
+    let handlebarsReadyRates = this.get('latestCurrencyRates');
+    handlebarsReadyRates.forEach(item => {
+      if (item.value === latesRates[item.currency]) {
+        item.hasFallen = null;
+      } else {
+        item.hasFallen = item.value < latesRates[item.currency];
+      }
+    });
+
+    this.set('currenciesFiltered', handlebarsReadyRates);
+    return;
   }
 });
