556113220161bc87082ad01f48c51e6d67df880e
Peter Janak <2010janak2010@gmail.com>
2016-12-14 00:08:49 +0000

    refactor: Add aria-hidden for icon returned by inflation-icon helper

--- app/helpers/inflation-icon.js
+++ app/helpers/inflation-icon.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export function inflationIcon (params) {
   if (params[0] === null) {
-    return Ember.String.htmlSafe('<i class="cur-rates-item-icon nochange"></i>');
+    return Ember.String.htmlSafe('<i class="cur-rates-item-icon nochange" aria-hidden="true"></i>');
   }
-  return params[0] ? Ember.String.htmlSafe('<i class="material-icons cur-rates-item-icon falling">arrow_downward</i>') : Ember.String.htmlSafe('<i class="material-icons cur-rates-item-icon rising">arrow_upward</i>');
+  return params[0] ? Ember.String.htmlSafe('<i class="material-icons cur-rates-item-icon falling" aria-hidden="true">arrow_downward</i>') : Ember.String.htmlSafe('<i class="material-icons cur-rates-item-icon rising" aria-hidden="true">arrow_upward</i>');
 }
 
 export default Ember.Helper.helper(inflationIcon);
