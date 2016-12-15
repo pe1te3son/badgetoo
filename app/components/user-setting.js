cfad7eaafca72479ad6171146e338d2c6285b20d
Peter Janak <2010janak2010@gmail.com>
2016-12-15 23:04:37 +0000

    refactor: Validate input for user setting within isValid property method

--- app/components/user-setting.js
+++ app/components/user-setting.js
@@ -3,6 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   editMode: false,
 
+  isValid: function () {
+    let value = this.get('value');
+    let pattern = this.get('pattern');
+
+    if (pattern) {
+      const reg = new RegExp(`${pattern}`);
+      return reg.test(value);
+    }
+
+    if (!value.length) {
+      return false;
+    }
+
+    return true;
+  }.property('value'),
+
   actions: {
     editMode (boolean) {
       this.set('editMode', boolean);
