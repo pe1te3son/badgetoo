162d17119f14ea95d55a85a65c57b6205a189ec3
Peter Janak <2010janak2010@gmail.com>
2016-12-18 23:12:10 +0000

    fix: Service worker syntax messed up. #Solved

--- public/sw.js
+++ public/sw.js
@@ -14,13 +14,11 @@
     event.respondWith(
       caches.match(event.request).then(function (resp) {
         return resp || fetch(event.request).then(function (response) {
-          caches.open(CACHE_VERSION).then(function (cache) {
+          return caches.open(CACHE_VERSION).then(function (cache) {
             cache.put(event.request, response.clone());
+            return response;
           });
-          return response;
         });
-      }).catch(function () {
-        console.log('not in cache');
       })
     );
   });
